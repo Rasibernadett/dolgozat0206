66866249e20e722647ea4252814d2358b665ee9a
Rasibernadett <150834700+Rasibernadett@users.noreply.github.com>
2024-02-06 18:57:54 +0100

    Add files via upload

---
+++ scriptRB.js
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-2">
+        <h1>Á.L.B. Ingatlanügyökség</h1>
+            <img src="c:\Users\diak\Desktop\dolgozatRasiBernadett\real-estate-agent.png" alt="Real Estate Agent" class="img-fluid"></img>
+        </div>
+        <div class="col-md-2">
+            <h2>Siofok, Fo u 4.</h2>
+            <p>Felújítandó nyaraló, közel a parthoz!</p>
+            <p>Hirdetés dátuma: 2023-06-04</p>
+            <p>Kategória: Nyaraló</p>
+            <p>Tehermentes: Igen</p>
+        </div>
+    </div>
+</div>
